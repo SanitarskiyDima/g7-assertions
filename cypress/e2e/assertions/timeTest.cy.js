cee6526e3ae457ecd9246495fe3a18d89d284359
DmytroSanitarskyi <dmytro.sanitarskyi@dataart.com>
2024-02-07 21:55:28 +0200

    example for managing different localisations in the site added

--- cypress/e2e/assertions/timeTest.cy.js
+++ cypress/e2e/assertions/timeTest.cy.js
@@ -1,5 +1,22 @@
+import textVariables from '../../fixtures/translations.json'
+
+let locale = Cypress.env('CYPRESS_LOCALIZATION');
+let strings = textVariables[locale];
+
+
 it('Time test', () => {
-    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+
+    cy.log(`Приклад як можна менеджити різні локалізації в тестах. 
+            Для того щоб змінити текст що вводиться в поля 
+            потрібно змінили локалізацію у файлі .env на одну з тих,
+            що вказані в файлі translations.json і перезапустити Сайпресс`);
+
+    cy.log(locale);
+    cy.log(strings);
+    cy.log(strings.registerButtonText);
+    cy.log(strings.loginButtonName);
+
+    cy.visit(`https://sanitarskyi-ngx-admin.herokuapp.com/`);
 
     cy.clock();
 
@@ -10,9 +27,9 @@ it('Time test', () => {
     cy.get('div.col-md-6.col-sm-12  button.select-button').eq(0).click();
     cy.get(`nb-option[ng-reflect-value="top-right"]`).click();
     cy.get(`input[name="title"]`).clear();
-    cy.get(`input[name="title"]`).type('qewqweqew');
+    cy.get(`input[name="title"]`).type(strings.registerButtonText);
     cy.get(`input[name="content"]`).clear();
-    cy.get(`input[name="content"]`).type('qweqweqwe');
+    cy.get(`input[name="content"]`).type(strings.loginButtonName);
     cy.get(`input[name="timeout"]`).clear();
     cy.get(`input[name="timeout"]`).type(60000);
     cy.contains('button', 'Show toast').click();
